727590d241f268a15e52d64d8e34255801358775
robdiggio <digiovanni.1816691@studenti.uniroma1.it>
2025-01-27 20:25:56 +0100

    attacco di poisoning per scambiare i label

---
+++ client.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from collections import OrderedDict
+from multiprocessing import Process, Manager
+from time import sleep
+import argparse
+import os
+import pickle
+import flwr as fl
+from flwr.client import NumPyClient
+import logging
+logging.getLogger("flwr").setLevel(logging.ERROR)
+
+if torch.backends.mps.is_available():
+    DEVICE = "mps"
+    DTYPE = torch.float32
+elif torch.cuda.is_available():
+    DEVICE = "cuda"
+    DTYPE = torch.float64
+else:
+    DEVICE = "cpu"
+    DTYPE = torch.float64
+
+ROUND = 1
+SEED = 7
+NUM_CLIENTS = 6
+
+# Argument parser
+parser = argparse.ArgumentParser(description="Flower Client for Federated Learning")
+parser.add_argument(
+    "--mode",
+    type=str,
+    choices=["legit", "attack", "poison"],
+    required=True,
+    help="Specify client mode: 'legit' or 'attack' or 'poison'",
+)
+args = parser.parse_args()
+mode = args.mode
+
+
+class CNN1DModel(nn.Module):
+    def __init__(self):
+        super(CNN1DModel, self).__init__()
+        self.conv1 = nn.Conv1d(50, 32, kernel_size=3, dtype=DTYPE)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(32, 50, dtype=DTYPE)
+        self.fc2 = nn.Linear(50, 3, dtype=DTYPE)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = self.flatten(x)
+        x = torch.relu(self.fc1(x))
+        x = torch.softmax(self.fc2(x), dim=1)
+        return x
+
+
+class FlowerClient(NumPyClient):
+    def __init__(self, client_id, mode, shared_dict, malicious):
+        super().__init__()
+        torch.manual_seed(SEED)
+        self.net = CNN1DModel().to(DEVICE)
+        self.client_id = client_id
+        self.trainloader, self.testloader = self.load_data()
+        self.mode = mode
+        self.shared_dict = shared_dict
+        self.malicious = malicious
+
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in self.net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(self.net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.as_tensor(v, dtype=DTYPE) for k, v in params_dict})
+        self.net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        round_number = config["server_round"]
+        self.train(round_number)
+        self.save_model_weights(round_number)
+        return self.get_parameters(config={}), len(self.trainloader.dataset), {}
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = self.test(self.testloader)
+        print(f"Client {self.client_id} Test Loss: {loss}, Test Accuracy: {accuracy}")
+        return float(loss), len(self.testloader), {"accuracy": float(accuracy)}
+
+    def save_model_weights(self, round_number):
+        filename = f"model_r{round_number}_s{SEED}.pt"
+        path = f"weights/{mode}/{filename}"
+        os.makedirs(f"weights/{mode}", exist_ok=True)
+        torch.save(self.get_parameters(None), path)
+        print(f"Saved model weights at {path}")
+
+    def load_data(self):
+        with open(f"Dataset/node_{self.client_id}.pkl", "rb") as f:
+            X_train, y_train, X_test, y_test, _, _ = pickle.load(f)
+        X_train_tensor = torch.as_tensor(X_train, dtype=DTYPE).to(DEVICE)
+        y_train_tensor = torch.as_tensor(y_train, dtype=DTYPE).to(DEVICE)
+        X_test_tensor = torch.as_tensor(X_test, dtype=DTYPE).to(DEVICE)
+        y_test_tensor = torch.as_tensor(y_test, dtype=DTYPE).to(DEVICE)
+        train_loader = DataLoader(
+            TensorDataset(X_train_tensor, y_train_tensor), batch_size=32, shuffle=True
+        )
+        test_loader = DataLoader(
+            TensorDataset(X_test_tensor, y_test_tensor), batch_size=32, shuffle=True
+        )
+        return train_loader, test_loader
+
+    def train(self, round_num):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(self.net.parameters(), lr=0.005)
+        self.net.train()
+        for epoch in range(5):
+            for inputs, labels in self.trainloader:
+                inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+                #Poison mode  - swap labels
+                if self.mode == "poison" and round_num > ROUND:
+                    # Trova le righe che corrispondono a [1., 0., 0.]
+                    mask1 = (labels == torch.tensor([1., 0., 0.], dtype=torch.float64)).all(dim=1)
+                    # Trova le righe che corrispondono a [0., 1., 0.]
+                    mask2 = (labels == torch.tensor([0., 1., 0.], dtype=torch.float64)).all(dim=1)
+                    # Sostituisci [1., 0., 0.] con [0., 1., 0.] e viceversa
+                    labels[mask1] = torch.tensor([0., 1., 0.], dtype=torch.float64)
+                    labels[mask2] = torch.tensor([1., 0., 0.], dtype=torch.float64)
+                optimizer.zero_grad()
+                outputs = self.net(inputs)
+                loss = criterion(outputs, torch.argmax(labels, dim=1))
+                loss.backward()
+                # Attack mode  - gradient sign flipping
+                if self.mode == "attack" and round_num > ROUND:
+                    for _, para in self.net.named_parameters():
+                        para.grad.data = -para.grad.data
+                optimizer.step()
+
+    def test(self, loader):
+        criterion = nn.CrossEntropyLoss()
+        correct, total, loss = 0, 0, 0.0
+        self.net.eval()
+        with torch.no_grad():
+            for data, target in loader:
+                data, target = data.to(DEVICE), target.to(DEVICE)
+                target = torch.argmax(target, dim=1)
+                outputs = self.net(data)
+                loss += criterion(outputs, target).item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += target.size(0)
+                correct += (predicted == target).sum().item()
+        loss /= len(loader.dataset)
+        accuracy = correct / total
+        return loss, accuracy
+
+
+def countdown(t):
+    for i in range(t, 0, -1):
+        print(i)
+        sleep(1)
+
+
+def client_wrapper(client_id, shared_dict, malicious):
+    client = FlowerClient(client_id, mode, shared_dict, malicious).to_client()
+    countdown(10)
+    fl.client.start_client(server_address="127.0.0.1:8080", client=client)
+
+
+if __name__ == "__main__":
+    manager = Manager()
+    shared_dict = manager.dict()
+    clients = []
+    for i in range(NUM_CLIENTS):
+        p = Process(target=client_wrapper, args=(i, shared_dict, mode == "attack"))
+        p.start()
+        clients.append(p)
+    for client in clients:
+        client.join()
